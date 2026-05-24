ae5fc21be306984babbe1f2cf93de95e8d9108ae
Yang <iignacius@outlook.com>
2026-05-24 21:16:32 +0300

    fix test file

--- tests/test_obj_loader.py
+++ tests/test_obj_loader.py
@@ -6,8 +6,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 
 class TestSystem:
-
-# T01-T04,T11-T18 need GUI,skip in githubCI
+    # T01-T04,T11-T18 need GUI,skip in githubCI
 
 
     def test_T05_parse_vertices(self):
